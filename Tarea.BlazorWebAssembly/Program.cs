749d85644c380310d769fe0762b79e65487c205b
Roberto <beto.jeria@gmail.com>
2022-01-08 12:52:59 -0300

    Agregamos 2do proyecto Blazor.WebAssembly
    Sabemos las diferencias entre PWA y una SPA, no obstante, en esta tarea no necesitaremos features de PWA, tales como notificaciones push, caching o instalación en el escritorio, por lo que simplemente creamos un proyecto que es igual al anterior.

---
+++ Tarea.BlazorWebAssembly/Program.cs
@@ -0,0 +1,30 @@
+using MatBlazor;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea.BlazorWebAssembly.Services;
+using Tarea.Shared.Interfaces;
+
+namespace Tarea.BlazorWebAssembly
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+            builder.Services.AddScoped<IApiClient, ApiClient>();
+            builder.Services.AddMatBlazor();
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ Tarea.BlazorWebAssembly/Services/ApiClient.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Tarea.Shared.ApiModels;
+using Tarea.Shared.Interfaces;
+
+namespace Tarea.BlazorWebAssembly.Services
+{
+    public class ApiClient : IApiClient
+    {
+        private readonly HttpClient _httpClient;
+        public ApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<ICollection<Region>> GetRegiones()
+        {
+            var response = await _httpClient.GetAsync("https://ailive-public.s3.amazonaws.com/regiones.json");
+            response.EnsureSuccessStatusCode();
+            var regiones = JsonConvert.DeserializeObject<ICollection<Region>>(await response.Content.ReadAsStringAsync());
+            return regiones.OrderBy(x => x.Nombre).ToList();
+        }
+    }
+}
