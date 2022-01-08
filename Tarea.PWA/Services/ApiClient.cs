c4a4cbfde7bd1c95766ae01a6583490a026bfa9b
Roberto <beto.jeria@gmail.com>
2022-01-07 23:20:48 -0300

    ApiClient.cs: Ordenar regiones por orden alfabético

--- Tarea.PWA/Services/ApiClient.cs
+++ Tarea.PWA/Services/ApiClient.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Tarea.Shared.ApiModels;
@@ -18,7 +19,8 @@ namespace Tarea.PWA.Services
         {
             var response = await _httpClient.GetAsync("https://ailive-public.s3.amazonaws.com/regiones.json");
             response.EnsureSuccessStatusCode();
-            return JsonConvert.DeserializeObject<ICollection<Region>>(await response.Content.ReadAsStringAsync());       
+            var regiones = JsonConvert.DeserializeObject<ICollection<Region>>(await response.Content.ReadAsStringAsync());
+            return regiones.OrderBy(x => x.Nombre).ToList();       
         }
     }
 }
