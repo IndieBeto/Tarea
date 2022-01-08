256ad91b52755f9320cbd9dd78cb0f8bc1ac6e3a
Roberto <beto.jeria@gmail.com>
2022-01-07 21:35:40 -0300

    Initial commit

---
+++ Tarea.Shared/ApiModels/Region.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Tarea.Shared.ApiModels
+{
+    public class Region
+    {
+        [JsonProperty("region")]
+        public string Nombre { get; set; }
+
+        [JsonProperty("comunas")]
+        public ICollection<string> Comunas { get; set; }
+    }
+}
